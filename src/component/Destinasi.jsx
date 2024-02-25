e2ecc682675e6e34612fa37b4fb66d3a5c4e1d9a
YHP125 <yohanesbrahmantya@gmail.com>
2024-02-25 10:30:30 +0700

    idk :)

--- src/component/Destinasi.jsx
+++ src/component/Destinasi.jsx
@@ -1,8 +1,22 @@
-import { Card, Col, Container, Row } from "react-bootstrap";
-import './Destinasi.css'; // Import your custom CSS if needed
-import destinations from '../assets/DataDestinasi.jsx'; // Import your destinations data
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Form,
+  FormControl,
+  InputGroup,
+  Row,
+} from "react-bootstrap";
+import "./Destinasi.css"; // Import your custom CSS if needed
+import destinations from "../assets/DataDestinasi.jsx"; // Import your destinations data
+import { useState } from "react";
 
 export default function Destinasi() {
+  const [inputValue, setinputValue] = useState("");
+  const handleInputChange = (event) => {
+    setinputValue(event.target.value);
+  };
   return (
     <Container fluid>
       <Row className="justify-content-center align-items-center">
@@ -15,6 +29,21 @@ export default function Destinasi() {
                     <strong>Destinasi Wisata</strong>
                   </h1>
                 </Card.Title>
+                <InputGroup className="mb-3">
+                  <Form.Control
+                    placeholder="Cari Destinasi"
+                    aria-label="Cari Destinasi"
+                    aria-describedby="basic-addon2"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                  />
+                  <Button
+                    variant="secondary"
+                    className="search-button"
+                    id="button-addon2">
+                    cari
+                  </Button>
+                </InputGroup>
                 <Card.Text>
                   Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 </Card.Text>
@@ -24,27 +53,39 @@ export default function Destinasi() {
         </Col>
       </Row>
       <Container>
-      <Row className="justify-content-center">        
-          {destinations.map(destination => (
-            <Col key={destination.id} xs={12} md={4} className="pb-3 w3-animate-bottom">
+        <Row className="justify-content-center">
+          {destinations.map((destination) => (
+            <Col
+              key={destination.id}
+              xs={12}
+              md={4}
+              className="pb-3 w3-animate-bottom">
               <Card>
-                <Card.Img variant="top" src={destination.image} style={{ 
-                  height: "200px", 
-                  objectFit: "cover"
-                 }}/>
-                <Card.Body style={{ 
-                  backgroundColor: "rgba(203, 203, 203, 1)"
-                 }}>
-                  <Card.Title className="destinasi-head-text">{destination.name}</Card.Title>
-                  <Card.Text className="destinasi-desc-text">{destination.description}</Card.Text>
-                {/* Tamabahin tombol detail yes */}
+                <Card.Img
+                  variant="top"
+                  src={destination.image}
+                  style={{
+                    height: "200px",
+                    objectFit: "cover",
+                  }}
+                />
+                <Card.Body
+                  style={{
+                    backgroundColor: "rgba(203, 203, 203, 1)",
+                  }}>
+                  <Card.Title className="destinasi-head-text">
+                    {destination.name}
+                  </Card.Title>
+                  <Card.Text className="destinasi-desc-text">
+                    {destination.description}
+                  </Card.Text>
+                  {/* Tamabahin tombol detail yes */}
                 </Card.Body>
               </Card>
             </Col>
           ))}
-      </Row>
+        </Row>
       </Container>
-      
     </Container>
   );
 }
